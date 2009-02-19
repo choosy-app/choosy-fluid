bd55626e6fa94c20ee9dcfd69c490402f707a6ca
George Brocklehurst <george.brocklehurst@gmail.com>
2009-02-19 05:04:19 +0000

    New Xcode project with basic UI set up

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ChoosyFluid
+//
+//  Created by George on 19/02/2009.
+//  Copyright George Brocklehurst 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
